b8be29dd9fa2ebc354e2dfd16de672e543b62d09
zhangyong <zhyebe@hotmail.com>
2023-02-15 13:09:11 +0800

    first-commit

---
+++ zy-utils/zy-utils.mjs
@@ -0,0 +1,196 @@
+import { defineComponent as P, toRefs as j, ref as u, computed as M, watch as ee, onBeforeMount as te, onBeforeUnmount as ae, onMounted as le, nextTick as ue, openBlock as oe, createElementBlock as ne } from "vue";
+const ve = P({
+  name: "vite-sign"
+}), re = /* @__PURE__ */ P({
+  ...ve,
+  props: {
+    width: {
+      type: Number,
+      default: 800
+    },
+    height: {
+      type: Number,
+      default: 300
+    },
+    lineWidth: {
+      type: Number,
+      default: 4
+    },
+    lineColor: {
+      type: String,
+      default: "#000000"
+    },
+    bgColor: {
+      type: String,
+      default: ""
+    },
+    isCrop: {
+      type: Boolean,
+      default: !1
+    },
+    isClearBgColor: {
+      type: Boolean,
+      default: !0
+    },
+    format: {
+      type: String,
+      default: "image/png"
+    },
+    quality: {
+      type: Number,
+      default: 1
+    }
+  },
+  emits: ["update:bgColor"],
+  setup(c, { expose: h, emit: w }) {
+    const D = c, {
+      height: C,
+      width: _,
+      bgColor: S,
+      lineWidth: X,
+      lineColor: U,
+      isCrop: W,
+      isClearBgColor: q,
+      format: L,
+      quality: O
+    } = j(D), x = u(!1), R = u(""), f = u([]), t = u(null), p = u(0), m = u(0), b = u(!1), n = u(1), a = u(), z = M(() => C.value / _.value);
+    M(() => {
+      var e;
+      return (e = a.value) == null ? void 0 : e.getBoundingClientRect();
+    });
+    const T = M(() => S.value ? S.value : "rgba(255, 255, 255, 0)");
+    ee(T, (e) => {
+      a.value.style.background = e;
+    }), te(() => {
+      window.addEventListener("resize", k);
+    }), ae(() => {
+      window.removeEventListener("resize", k);
+    }), le(() => {
+      ue(() => {
+        a.value.height = C.value, a.value.width = _.value, a.value.style.background = T.value, k(), document.onmouseup = () => {
+          b.value = !1;
+        };
+      });
+    });
+    const k = () => {
+      a.value.style.width = _.value + "px";
+      const e = parseFloat(window.getComputedStyle(a.value).width);
+      a.value.style.height = z.value * e + "px", t.value = a.value.getContext("2d"), t.value.scale(1 * n.value, 1 * n.value), n.value = e / _.value, t.value.scale(1 / n.value, 1 / n.value);
+    }, J = (e) => {
+      e = e || event, e.preventDefault(), b.value = !0, x.value = !0;
+      let l = {
+        x: e.offsetX,
+        y: e.offsetY
+      };
+      Y(l);
+    }, F = (e) => {
+      if (e = e || event, e.preventDefault(), b.value) {
+        let l = {
+          x: e.offsetX,
+          y: e.offsetY
+        };
+        I(l);
+      }
+    }, A = (e) => {
+      e = e || event, e.preventDefault();
+      let l = {
+        x: e.offsetX,
+        y: e.offsetY
+      };
+      E(l), b.value = !1;
+    }, H = (e) => {
+      if (e = e || event, e.preventDefault(), x.value = !0, e.touches.length === 1) {
+        let l = {
+          x: e.targetTouches[0].clientX - a.value.getBoundingClientRect().left,
+          y: e.targetTouches[0].clientY - a.value.getBoundingClientRect().top
+        };
+        Y(l);
+      }
+    }, Q = (e) => {
+      if (e = e || event, e.preventDefault(), e.touches.length === 1) {
+        let l = {
+          x: e.targetTouches[0].clientX - a.value.getBoundingClientRect().left,
+          y: e.targetTouches[0].clientY - a.value.getBoundingClientRect().top
+        };
+        I(l);
+      }
+    }, V = (e) => {
+      if (e = e || event, e.preventDefault(), e.touches.length === 1) {
+        let l = {
+          x: e.targetTouches[0].clientX - a.value.getBoundingClientRect().left,
+          y: e.targetTouches[0].clientY - a.value.getBoundingClientRect().top
+        };
+        E(l);
+      }
+    }, Y = (e) => {
+      p.value = e.x, m.value = e.y, t.value.beginPath(), t.value.moveTo(p.value, m.value), t.value.lineTo(e.x, e.y), t.value.lineCap = "round", t.value.lineJoin = "round", t.value.lineWidth = X.value * n.value, t.value.stroke(), t.value.closePath(), f.value.push(e);
+    }, I = (e) => {
+      t.value.beginPath(), t.value.moveTo(p.value, m.value), t.value.lineTo(e.x, e.y), t.value.strokeStyle = U.value, t.value.lineWidth = X.value * n.value, t.value.lineCap = "round", t.value.lineJoin = "round", t.value.stroke(), t.value.closePath(), m.value = e.y, p.value = e.x, f.value.push(e);
+    }, E = (e) => {
+      t.value.beginPath(), t.value.moveTo(p.value, m.value), t.value.lineTo(e.x, e.y), t.value.lineCap = "round", t.value.lineJoin = "round", t.value.stroke(), t.value.closePath(), f.value.push(e), f.value.push({ x: -1, y: -1 });
+    }, Z = (e) => {
+      let l = e && e.format ? e.format : L.value, v = e && e.quality ? e.quality : O.value;
+      return new Promise((d, r) => {
+        if (!x.value) {
+          r("Warning: Not Signned!");
+          return;
+        }
+        var o = t.value.getImageData(
+          0,
+          0,
+          a.value.width,
+          a.value.height
+        );
+        t.value.globalCompositeOperation = "destination-over", t.value.fillStyle = T.value, t.value.fillRect(0, 0, a.value.width, a.value.height), R.value = a.value.toDataURL(l, v);
+        let i = R.value;
+        if (t.value.clearRect(0, 0, a.value.width, a.value.height), t.value.putImageData(o, 0, 0), t.value.globalCompositeOperation = "source-over", W.value) {
+          const g = G(o.data);
+          let s = document.createElement("canvas");
+          const B = s.getContext("2d");
+          s.width = g[2] - g[0], s.height = g[3] - g[1];
+          const K = t.value.getImageData(...g);
+          B.globalCompositeOperation = "destination-over", B.putImageData(K, 0, 0), B.fillStyle = T.value, B.fillRect(0, 0, s.width, s.height), i = s.toDataURL(l, v), s = null;
+        }
+        d(i);
+      });
+    }, $ = () => {
+      t.value.clearRect(0, 0, a.value.width, a.value.height), q.value && (w("update:bgColor", ""), a.value.style.background = "rgba(255, 255, 255, 0)"), f.value = [], x.value = !1, R.value = "";
+    }, G = (e) => {
+      for (var l = a.value.width, v = 0, y = a.value.height, d = 0, r = 0; r < a.value.width; r++)
+        for (var o = 0; o < a.value.height; o++) {
+          var i = (r + a.value.width * o) * 4;
+          (e[i] > 0 || e[i + 1] > 0 || e[i + 2] || e[i + 3] > 0) && (d = Math.max(o, d), v = Math.max(r, v), y = Math.min(o, y), l = Math.min(r, l));
+        }
+      return l++, v++, y++, d++, [l, y, v, d];
+    };
+    return h({
+      reset: $,
+      generate: Z
+    }), (e, l) => (oe(), ne("canvas", {
+      ref_key: "canvas",
+      ref: a,
+      onMousedown: J,
+      onMousemove: F,
+      onMouseup: A,
+      onTouchstart: H,
+      onTouchmove: Q,
+      onTouchend: V
+    }, null, 544));
+  }
+});
+const ie = (c, h) => {
+  const w = c.__vccOpts || c;
+  for (const [D, C] of h)
+    w[D] = C;
+  return w;
+}, N = /* @__PURE__ */ ie(re, [["__scopeId", "data-v-36659fdb"]]), se = [N], de = {
+  install(c) {
+    se.forEach((h) => {
+      c.component(h.name, N);
+    });
+  }
+};
+export {
+  N as ViteSign,
+  de as default
+};
